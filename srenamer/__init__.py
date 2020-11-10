37dd858141f7eae04d3a84c795f498f6cde2ddda
Maxim Makovskiy <makovskiyms@gmail.com>
2020-11-10 14:51:08 +0600

    Make application out of this mess

--- srenamer/__init__.py
+++ srenamer/__init__.py
@@ -1,6 +1,5 @@
 from .renamer import Renamer
-from .tmdb_api import MovieDatabase 
+from .tmdb_api import MovieDatabase
 
-
-__all__ = ['Renamer', 'MovieDatabase']
-
+__all__ = ["Renamer", "MovieDatabase"]
+__version__ = "0.1.0"
--- srenamer/renamer.py
+++ srenamer/renamer.py
@@ -7,17 +7,17 @@ class Renamer:
 
     @classmethod
     def getwd(cls):
-        return cls(input('Enter path to the directory: '))
+        return cls(input("Enter path to the directory: "))
 
     def rename(self, series_list):
         path = os.getcwd()
-        dir_list = next(os.walk('.'))[2]
+        dir_list = next(os.walk("."))[2]
         if dir_list:
             for src, dst in zip(dir_list, series_list):
                 ext = os.path.splitext(src)[1]
-                os.rename(os.path.join(path, src), os.path.join(path, 
-                    ''.join([dst, ext])))
-            print('Done!')
+                os.rename(
+                    os.path.join(path, src), os.path.join(path, "".join([dst, ext]))
+                )
+            print("Done!")
         else:
             print("Directory", path, "is empty.")
-
